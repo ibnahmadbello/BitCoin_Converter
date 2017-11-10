4820760b253c7c8a2d285bfc36e99a43d4d67005
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2017-11-10 11:01:49 +0100

    make the fab clickable

--- app/src/main/java/com/regent/tech/bitcoin_converter/MainActivity.java
+++ app/src/main/java/com/regent/tech/bitcoin_converter/MainActivity.java
@@ -2,6 +2,7 @@ package com.regent.tech.bitcoin_converter;
 
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,11 +20,20 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Adding Card", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
