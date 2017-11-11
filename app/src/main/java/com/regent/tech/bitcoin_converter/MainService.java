c262df2b259317342909377f3091f214fc009467
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2017-11-11 01:51:48 +0100

    Updated the MainService file

---
+++ app/src/main/java/com/regent/tech/bitcoin_converter/MainService.java
@@ -0,0 +1,18 @@
+package com.regent.tech.bitcoin_converter;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by root on 11/11/17.
+ */
+
+interface MainService {
+    @GET("price")
+    Call<ResponseBody> exchangeRate(
+            @Query("fsym") String crypto,
+            @Query("tsysm") String other
+    );
+}
